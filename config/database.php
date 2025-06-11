0a7a7265c1cff5aabff9449c5618376ae6b3ffcd
ArisZajti <aris.zajti01@gmail.com>
2025-06-11 14:44:51 +0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -27,6 +27,18 @@ if (getenv('JAWSDB_URL')) {
     }
 }
 
+// Debug: log DB connection values to storage/logs/laravel.log
+if (function_exists('logger')) {
+    logger('DB debug', [
+        'DB_CONNECTION' => env('DB_CONNECTION'),
+        'DB_HOST' => env('DB_HOST'),
+        'DB_PORT' => env('DB_PORT'),
+        'DB_DATABASE' => env('DB_DATABASE'),
+        'DB_USERNAME' => env('DB_USERNAME'),
+        'JAWSDB_URL' => getenv('JAWSDB_URL'),
+    ]);
+}
+
 return [
     /*
     |--------------------------------------------------------------------------
